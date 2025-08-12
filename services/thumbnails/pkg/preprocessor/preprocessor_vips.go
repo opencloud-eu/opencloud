8d3874a3b403fe2f03ca37b2ae2a9e0f80b16bd5
Thomas Heijligen <src@posteo.de>
2025-08-12 19:48:11 +0200

    thumbnails: vips: do not fail on first errer
    This allows the thumbnail generation for images with invalid SOS parameters

--- services/thumbnails/pkg/preprocessor/preprocessor_vips.go
+++ services/thumbnails/pkg/preprocessor/preprocessor_vips.go
@@ -19,7 +19,8 @@ type ImageDecoder struct{}
 
 func (v ImageDecoder) Convert(r io.ReadCloser) (interface{}, error) {
 	src := vips.NewSource(r)
-	return vips.NewImageFromSource(src, nil)
+	// This fiexs the SOS invalid length but might be a security nightmare
+	return vips.NewImageFromSource(src, &vips.LoadOptions{FailOnError: false})
 
 	// Alternative with vips 1.8+
 	// First test the RAW decoder, this is not implemented in NewImageFromSrc
