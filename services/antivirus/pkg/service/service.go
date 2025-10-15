owncloud/ocis

fix: linter issue

services/antivirus/pkg/service/service.go
@@ -131,7 +131,7 @@ func (av Antivirus) Run() error {
 	}
 
 	wg := sync.WaitGroup{}
-	for i := 0; i < av.c.Workers; i++ {
+	for range av.c.Workers {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()

opencloud-eu/opencloud

services/antivirus/pkg/service/service.go
@@ -138,7 +138,7 @@ func (av Antivirus) Run() error {
 	}
 
 	wg := sync.WaitGroup{}
-	for i := 0; i < av.config.Workers; i++ {
+	for range av.config.Workers {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()