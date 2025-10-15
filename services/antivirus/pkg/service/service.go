owncloud/ocis

chore: change constant name to camelcase

services/antivirus/pkg/service/service.go
@@ -108,7 +108,7 @@ func (av Antivirus) Run() error {
 		av.c.Events.TLSInsecure = false
 	}
 
-	connName := generators.GenerateConnectionName(av.c.Service.Name, generators.NTYPE_BUS)
+	connName := generators.GenerateConnectionName(av.c.Service.Name, generators.NTypeBus)
 	natsStream, err := stream.NatsFromConfig(connName, false, stream.NatsConfig(av.c.Events))
 	if err != nil {
 		return err

opencloud-eu/opencloud

services/antivirus/pkg/service/service.go
@@ -115,7 +115,7 @@ func (av Antivirus) Run() error {
 		av.config.Events.TLSInsecure = false
 	}
 
-	connName := generators.GenerateConnectionName(av.config.Service.Name, generators.NTYPE_BUS)
+	connName := generators.GenerateConnectionName(av.config.Service.Name, generators.NTypeBus)
 	natsStream, err := stream.NatsFromConfig(connName, false, stream.NatsConfig(av.config.Events))
 	if err != nil {
 		return err